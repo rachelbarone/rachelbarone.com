0b7d1f9f8f2b34d0f7f7cfacdec4efdb7c384d76
Timothy Levi Campbell <tlc9406@gmail.com>
2014-05-21 14:48:24 -0400

    Updates mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -4,11 +4,13 @@ class MessageMailer < ActionMailer::Base
   def new_message(message)
     @message = message
     mail(
-      :to => "Rachel Barone <info@rachelbarone.com>",
-      :subject => "New Message from #{message.first_name} #{message.last_name}"
+      #:to => "Rachel Barone <info@rachelbarone.com>",
+      :to => "Tim Campbell <tlc9406@gmail.com>",
+      :subject => "New Message from #{message.first_name} #{message.last_name}",
+      :reply_to => "#{message.first_name} #{message.last_name} <#{message.email}>"
     )
   end
-  
+
   def confirmation(message)
     mail(
       :to => "#{message.first_name} #{message.last_name} <message.email>",
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,22 @@
 class Message < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :city,
     :state, :country, :phone, :referrer, :body
-    
+
   validates :email, :first_name, :last_name, :body, presence: true
+
+  def list_attributes
+    html = ""
+
+    self.attributes.each do |k,v|
+      if k == "email"
+        html += "<b>#{k.humanize}:</b>&emsp;<a href='#{v}'>#{v}</a>"
+      else
+        html += "<b>#{k.humanize}:</b>&emsp;#{v}"
+      end
+
+      html += "<br>"
+    end
+
+    return html
+  end
 end
